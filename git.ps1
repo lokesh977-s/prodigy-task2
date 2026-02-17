a41a70c2a6ca91b2563e3941543713c67bff9c27
lokesh977-s <lokesridhar@gmail.com>
2026-02-17 15:07:36 +0530

    first commit

--- git.ps1
+++ git.ps1
@@ -2,5 +2,5 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/lokesh977-s/prodigy-task2.git
+git remote add origin https://github.com/lokesh977-s/prodigy-3.git
 git push -u origin main
